bf9996c7bb5ef7d1bea45e9503ddaa7d836810af
iain <iain@soulflyer.co.uk>
2016-01-18 12:31:45 +0700

    responds to ENTER key

--- src/find_pics/core.clj
+++ src/find_pics/core.clj
@@ -1,11 +1,17 @@
 (ns find-pics.core
-  (:use     [seesaw core tree]
-            [find-images.core :exclude (-main)])
-  (:import  [java.io File])
-  (:require [monger.collection :as mc]
-            [monger.core :as mg]
-            [monger.operators :refer :all])
-  (:gen-class))
+  (:import java.io.File)
+  (:require [image-lib.core :refer [find-images
+                                    image-path
+                                    get-best-image]]
+            [monger
+             [collection :as mc]
+             [core :as mg]]
+            [seesaw
+             [core :refer :all]
+             [tree :refer :all]
+             [keymap :refer :all]])
+;;  (:gen-class)
+  )
 
 (def database "soulflyer")
 (def keyword-collection "keywords")
@@ -32,27 +38,22 @@
   [keyword-name]
   (first (mc/find-maps db keyword-collection {:_id keyword-name})))
 
-(defn get-best-image
-  [given-keyword]
-  (thumbnail-file
-   (image-path
-    (last
-     (sort-by :Rating (find-images database images-collection :Keywords given-keyword))))))
-
 (defn sample-thumbnail
   "given a keyword returns the thumbnail image sample File."
   [given-keyword]
   (let [kw (get-keyword given-keyword)]
-    (if (:sample kw)
-      (thumbnail-file (:sample kw))
-      (get-best-image given-keyword))))
+    (thumbnail-file
+     (if (:sample kw)
+       (:sample kw)
+       (get-best-image database images-collection given-keyword)))))
+
+(defn get-image-list
+  [given-keyword]
+  (find-images database images-collection "Keywords" given-keyword))
 
 (defn branch?
   [keyword-node]
-  ;;(complement (nil? (keyword-node :sub)))
-  (< 0 (count (keyword-node :sub)))
-  ;;true
-  )
+  (< 0 (count (keyword-node :sub))))
 
 (defn get-children
   [keyword]
@@ -67,40 +68,70 @@
 (defn render-file-item
   [renderer {:keys [value]}]
   (config! renderer
-          :text (:_id value)
-          ;;:icon default-thumbnail
-          ))
+          :text (:_id value)))
+
+(defn keypress [e]
+  (let [k (.getKeyChar e)]
+    (prn k (type k))
+    (if (= k \newline)
+      (alert "ENTER!")
+      (prn "some other key"))))
 
 (defn make-frame []
   (frame
    :title "Keyword Explorer"
    :size [1400 :by 800]
+   ;;:listen [:key-typed keypress]
    ;; :width 800
    ;; :height 600
    :content
 
    (border-panel
+    ;; :listen [:key-typed keypress]
     :center (left-right-split
-             (scrollable (tree    :id :tree :model tree-model :renderer render-file-item))
+             (scrollable
+              (tree    :id :tree
+                       :model tree-model
+                       :renderer render-file-item))
              (scrollable
               (label :id :image
                      :icon alternate-thumbnail
-                     :text "errmmmmm"
+                     :text "errmmmmm, no"
                      :valign :top
                      :v-text-position :bottom
                      :h-text-position :center))
              :divider-location 7/8)
     :south  (label :id :status :text "Ready"))))
 
+(defn make-frame2 []
+  (frame
+   :title "Keyword Explorer"
+   :size [1400 :by 800]
+   :content (scrollable
+             ;; (label :text "uh?")
+             (tree    :id :tree
+                      :model tree-model
+                      :renderer render-file-item)
+             )))
+
+
+(defn details-handler
+  [e]
+  (alert "Pressed ENTER"))
+
 (defn -main [& args]
   (invoke-later
-   (let
-       [f (make-frame)]
+   (let [f (make-frame)
+         handler (fn [e] (alert "pressed key"))]
+     (map-key f "ENTER" details-handler)
      (listen
-      (select f [:#tree]) :selection
+      (select f [:#tree])
+      :selection
       (fn [e]
         (if-let [kw (last (selection e))]
-          (let
-            [files (get-children kw)]
-            (config! (select f [:#image]) :icon (sample-thumbnail (:_id kw)))))))
+          (let [files (get-children kw)]
+            (config! (select f [:#image])
+                     :icon (sample-thumbnail (:_id kw))
+                     ;; :text (find-images database images-collection "Keywords" (:_id kw))
+                     )))))
      (show! f))))
